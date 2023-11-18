33f98db924241b2c67b77d366f93cb207b7c5a09
Shakil53 <shakilahmed.dcc@gmail.com>
2023-11-18 21:26:07 +0600

    server on properly

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,12 @@ import config from './app/config';
 // const mongoose = require('mongoose');
 import mongoose from 'mongoose';
 
-main().catch((err) => console.log(err));
+
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
+    app.listen(config, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
